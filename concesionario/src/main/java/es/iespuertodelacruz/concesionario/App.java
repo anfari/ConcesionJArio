2ddfbdc01c6de2804c61fc293088940438eb7274
joel92MM <joel_tf_92@hotmail.com>
2021-04-21 15:42:43 +0100

    cambios en la llave

--- concesionario/src/main/java/es/iespuertodelacruz/concesionario/App.java
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/App.java
@@ -1,7 +1,6 @@
 package es.iespuertodelacruz.concesionario;
 
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello Wod!" );
