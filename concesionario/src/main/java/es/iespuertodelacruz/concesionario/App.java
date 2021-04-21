6ba2165579e2fca4ef2ed76321aa80e04a0bc9e4
joel92MM <joel_tf_92@hotmail.com>
2021-04-21 15:25:51 +0100

    prueba cambios

--- concesionario/src/main/java/es/iespuertodelacruz/concesionario/App.java
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/App.java
@@ -1,10 +1,9 @@
 package es.iespuertodelacruz.concesionario;
 
-
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Wod!" );
     }
 }
