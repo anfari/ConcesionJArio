d0e9926f3e9fee164f4d0c9a9510b224aef890de
joel92MM <joel_tf_92@hotmail.com>
2021-04-21 15:46:53 +0100

    Se crea las clases de la api

---
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/api/Persona.java
@@ -0,0 +1,10 @@
+package es.iespuertodelacruz.concesionario.api;
+
+public class Persona {
+    String nombre;
+    String apellidos;
+    String dni;
+    String fechaNacimiento;
+    String telefono;
+    Direccion direccion;
+}
---
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/api/Vendedor.java
@@ -0,0 +1,5 @@
+package es.iespuertodelacruz.concesionario.api;
+
+public class Vendedor {
+    
+}
