1f5f58cd3e95faacd5fe55762eb18e97a8a0d97f
joel92MM <joel_tf_92@hotmail.com>
2021-05-07 12:50:09 +0100

    Se crea clases nuevas

---
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/modelo/ClienteModelo.java
@@ -0,0 +1,43 @@
+package es.iespuertodelacruz.concesionario.modelo;
+/**
+ * Clase CienteModelo 
+ */
+public class ClienteModelo {
+    /**
+     * Metodo que inserta en una lista
+     * @param cliente inserta el cliente
+     */
+    public void insertar(Cliente cliente){
+
+    }
+     /**
+     * Metodo que modifica en una lista
+     * @param clienteAlmacenado cliente almancenar
+     * @param clienteNuevo cliente nuevo
+     */
+    public void modificar(Cliente clienteAlmacenado, Cliente clienteNuevo){
+
+    }
+     /**
+     * Metodo que busca un cliente en la lista
+     * @param cliente a buscar
+     */
+    public void buscar(Cliente cliente){
+
+    }
+     /**
+     * Metodo que elimina un cliente de la lista
+     * @param cliente cliente a eliminar
+     */
+    public void eliminar(Cliente cliente){
+
+    }
+    /**
+     * Metodo que verifica si existe en la lista
+     * @param cliente cliente a verificar 
+     * @return true o false
+     */
+    public boolean existe(Cliente cliente){
+
+    }  
+}
---
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/modelo/VehiculoModelo.java
@@ -0,0 +1,43 @@
+package es.iespuertodelacruz.concesionario.modelo;
+/**
+ * Clase VehiculoModelo 
+ */
+public class VehiculoModelo {
+    /**
+     * Metodo que inserta en una lista
+     * @param vehiculo inserta el vehiculo
+     */
+    public void insertar(Vehiculo vehiculo){
+
+    }
+     /**
+     * Metodo que modifica en una lista
+     * @param vehiculoAlmacenado vehiculo almancenar
+     * @param vehiculoNuevo vehiculo nuevo
+     */
+    public void modificar(Vehiculo vehiculoAlmacenado, Vehiculo vehiculoNuevo){
+
+    }
+     /**
+     * Metodo que busca un vehiculo en la lista
+     * @param vehiculo a buscar
+     */
+    public void buscar(Vehiculo vehiculo){
+
+    }
+     /**
+     * Metodo que elimina un vehiculo de la lista
+     * @param vehiculo vehiculo a eliminar
+     */
+    public void eliminar(Vehiculo vehiculo){
+
+    }
+    /**
+     * Metodo que verifica si existe en la lista
+     * @param vehiculo vehiculo a verificar 
+     * @return true o false
+     */
+    public boolean existe(Vehiculo vehiculo){
+
+    }
+}
