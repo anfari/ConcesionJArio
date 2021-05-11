376fc4aa57e7e1193d81198fb502decdc8b350ec
anfari <antonio.27fo@gmail.com>
2021-05-11 18:30:38 +0100

    Añadir identificador

--- concesionario/src/main/java/es/iespuertodelacruz/concesionario/api/Direccion.java
+++ concesionario/src/main/java/es/iespuertodelacruz/concesionario/api/Direccion.java
@@ -3,6 +3,7 @@ package es.iespuertodelacruz.concesionario.api;
  * Clase direccion que contiene la direccion de una persona
  */
 public class Direccion {
+    String identificador;
     String calle;
     int numero;
     String codigoPostal;
@@ -11,22 +12,40 @@ public class Direccion {
 
     /**
      * Constructor de direccion con parametros
+     * @param identificador identificador de la direccion
      * @param calle nombre de la calle 
      * @param numero numeo de la casa
      * @param codigoPostal codigo postal 
      * @param provincia provincia de residencia
      * @param poblacion poblacion de residencia
      */
-    public Direccion(String calle, int numero, String codigoPostal, 
+    public Direccion(String identificador, String calle, int numero, String codigoPostal, 
     String provincia, String poblacion) {
+        this.identificador = identificador;
         this.calle = calle;
         this.numero = numero;
         this.codigoPostal = codigoPostal;
         this.provincia = provincia;
         this.poblacion = poblacion;
     }
-    //Aqui van los getter y setter
+
    
+    /**
+     * Getter de la clase Direccion
+     * @return identificador de la direccion
+     */
+    public String getIdentificador() {
+        return this.identificador;
+    }
+
+    /**
+     * Setter de la clase Direccion
+     * @param identificador identificador de la direccion
+     */
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
     /**
      * Getter de la clase Direccion
      * @return nombre de la calle
@@ -34,27 +53,31 @@ public class Direccion {
     public String getCalle() {
         return this.calle;
     }
+
     /**
-     * setter de la clase Direccion
+     * Setter de la clase Direccion
      * @param calle nombre de la calle
      */
     public void setCalle(String calle) {
         this.calle = calle;
     }
+
     /**
      * Getter de la clase Direccion
-     * @return numero de la calle
+     * @return numero de la casa
      */
     public int getNumero() {
         return this.numero;
     }
+
      /**
-     * setter de la clase Direccion
-     * @param numero numero de la calle
+     * Setter de la clase Direccion
+     * @param numero numero de la casa
      */
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
     /**
      * Getter de la clase Direccion
      * @return codigo postal de la calle
@@ -62,35 +85,40 @@ public class Direccion {
     public String getCodigoPostal() {
         return this.codigoPostal;
     }
+
     /**
-     * setter de la clase Direccion
+     * Setter de la clase Direccion
      * @param codigoPostal codigo postal de la calle
      */
     public void setCodigoPostal(String codigoPostal) {
         this.codigoPostal = codigoPostal;
     }
+
     /**
-     * getter de la clase Direccion
+     * Getter de la clase Direccion
      * @return provincia del cliente
      */
     public String getProvincia() {
         return this.provincia;
     }
-      /**
+
+    /**
      * setter de la clase Direccion
      * @param provincia del cliente
      */
     public void setProvincia(String provincia) {
         this.provincia = provincia;
     }
-      /**
+
+    /**
      * getter de la clase Direccion
      * @return  poblacion 
      */
     public String getPoblacion() {
         return this.poblacion;
     }
-      /**
+
+    /**
      * setter de la clase Direccion
      * @param poblacion nombre
      */
