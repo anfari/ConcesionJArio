c12e26b40ae8f9fe526a20e0f392742ff66c8829
joel92MM <joel_tf_92@hotmail.com>
2021-05-20 17:10:25 +0100

    Se añade codigo #12

--- concesionario/src/resources/sqlite/persona_insertar.sql
+++ concesionario/src/resources/sqlite/persona_insertar.sql
@@ -5,26 +5,26 @@ VALUES("11234567D","Marcelino","Manzano","13/05/1994","+34653321432","I018");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
 VALUES("12234567D","Pastor","Gimenez","02/02/1988","+34653321432","I017");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("13234567D","Perico","Afonso","23/10/1989","+34653321432","I001");
+VALUES("13234567D","Perico","Afonso","23/10/1989","+34653321432","11111111A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("14234567D","Anastasia","Real","03/02/1988","+34653321432","I002");
+VALUES("14234567D","Anastasia","Real","03/02/1988","+34653321432","11111112A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("15234567D","Castolo","Rajado","03/02/1981","+34653321432","I000");
+VALUES("15234567D","Castolo","Rajado","03/02/1981","+34653321432","11111113A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("16234567D","Lolita","Zuzunaga","03/02/1994","+34653321432","I003");
+VALUES("16234567D","Lolita","Zuzunaga","03/02/1994","+34653321432","11111114A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("17234567D","Encarnacion","Bonachera","03/02/1991","+34653321432","I004");
+VALUES("17234567D","Encarnacion","Bonachera","03/02/1991","+34653321432","11111115A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("18234567D","Maria","Pieldelobo","13/02/1994","+34653321432","I005");
+VALUES("18234567D","Maria","Pieldelobo","13/02/1994","+34653321432","11111116A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("19234567D","Cucura","Pieldecordero","22/05/1995","+34653321432","I006");
+VALUES("19234567D","Cucura","Pieldecordero","22/05/1995","+34653321432","11111117A");
 
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("11111111A","Hierbora","Víbora","03/02/1989","+34653321432","I007");
+VALUES("11111111A","Hierbora","Víbora","03/02/1989","+34653321432","11111118A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("11111112A","Rita","Oreja de perro","03/02/1988","+34653321432","I008");
+VALUES("11111112A","Rita","Oreja de perro","03/02/1988","+34653321432","11111119A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
-VALUES("11111113A","Perica","Rabo de gato","03/02/1987","+34653321432","I009");
+VALUES("11111113A","Perica","Rabo de gato","03/02/1987","+34653321432","11111110A");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
 VALUES("11111114A","Lucas","Bigotes","03/02/1994","+34653321432","I010");
 INSERT INTO Persona (dni,nombre, apellidos,fechaNacimiento, telefono, direccion) 
